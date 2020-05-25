4a1c7967b88559c156d28b2e3ac54b846eb3fb15
YHS <yhsdeveloper2551@gmail.com>
2020-05-26 04:45:59 +0900

    Updates

---
+++ precache-manifest.c6b056dfc31d4e91dbf2f7aca35de8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8d168fb8010e19b7d76062f12fe1d5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e1cc5259f67c7ffdb51b",
+    "url": "/monsters-rolodex/static/css/main.dfb70c35.chunk.css"
+  },
+  {
+    "revision": "1f56406bc7fcb2d1baa5",
+    "url": "/monsters-rolodex/static/js/2.b8670880.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b8670880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1cc5259f67c7ffdb51b",
+    "url": "/monsters-rolodex/static/js/main.86592ef4.chunk.js"
+  },
+  {
+    "revision": "20b97ff4eb860dd1ef94",
+    "url": "/monsters-rolodex/static/js/runtime-main.79186a7c.js"
+  }
+]);
